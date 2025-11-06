90a110ce81e6cbd3aa39d58de29a9d5cad72d182
Alreem Alothman <alreemothman66@gmail.com>
2025-11-06 20:48:26 +0300

    V6 Local Player

--- Assets/Scripts/KitchenTable/Counters/StoveCounter.cs
+++ Assets/Scripts/KitchenTable/Counters/StoveCounter.cs
@@ -103,7 +103,6 @@ public class StoveCounter : BaseCounter, IHasProgress
             case State.Burned:
                 break;
         }
-        Debug.Log(state);
     }
 
     public override void Interact(PlayerMovement player)
