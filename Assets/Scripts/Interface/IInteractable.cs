3fe0ac2f82a031493e12ae3aea4cdf7ae2d4808a
Alreem Alothman <alreemothman66@gmail.com>
2025-11-09 16:44:19 +0300

    undo

--- Assets/Scripts/Interface/IInteractable.cs
+++ Assets/Scripts/Interface/IInteractable.cs
@@ -1,6 +1,10 @@
-using UnityEngine;
-
 public interface IInteractable
 {
+<<<<<<< HEAD
     void Interact(GameObject interactor);
 }
+=======
+    void Interact(PlayerMovement player);
+    void InteractAlterante(PlayerMovement player);
+}
+>>>>>>> parent of 91e985d (Merge remote-tracking branch 'origin/Naila' into Alreem)
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,11 +1,26 @@
+using System;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody))]
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour, IKitchenObjectParant
 {
     [Header("References")]
     [SerializeField] private Transform cameraTransform;
+    [SerializeField] private float interactRange = 2f;
+    private Vector3 lastIntaractinDir;
+    [SerializeField] private LayerMask interactLayerMask;
+    private IInteractable SelectedCounter;
+    public event EventHandler<SelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+    public class SelectedCounterChangedEventArgs : EventArgs
+    {
+        public IInteractable selectedCounter;
+    }
+    private KitchenObject kitchenObject;
+    [SerializeField] private Transform holdPoint;         // ‰ﬁÿ… «·Ìœ
+
+
 
     [Header("Move")]
     [SerializeField] private float walkSpeed = 5f;
@@ -17,12 +32,15 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private float groundDistanceCheck = 0.3f;
     [SerializeField] private float rayStartOffset = 0.06f;
+    
 
     [Header("Crouch")]
     [SerializeField] private bool useToggleCrouch = true;
     [SerializeField] private float crouchSpeed = 2.5f;
     [SerializeField] private float standingHeight = 2.0f;
     [SerializeField] private float crouchHeight = 1.0f;
+
+    [SerializeField] private PlayerInput playerInput; 
 
     private Vector3 planarMoveDir;
     private Rigidbody rb;
@@ -31,14 +49,18 @@ public class PlayerMovement : MonoBehaviour
     private bool isGrounded;
     private bool isSprinting;
     private bool isCrouching;
+    public clearCounterInteraction clearCounterInteraction;
+
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
         capsule = GetComponent<CapsuleCollider>();
 
+
         rb.interpolation = RigidbodyInterpolation.Interpolate;
         rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+
     }
 
     private void FixedUpdate()
@@ -50,6 +72,58 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 rayOrigin = transform.position + Vector3.up * rayStartOffset;
         isGrounded = Physics.Raycast(rayOrigin, Vector3.down, groundDistanceCheck, groundLayer, QueryTriggerInteraction.Ignore);
+
+    }
+
+    private void HandelInteraction()
+    {
+        if (moveInput != Vector2.zero)
+        {
+            lastIntaractinDir = transform.forward;
+        }
+
+        if (Physics.Raycast(transform.position, lastIntaractinDir, out RaycastHit raycastHit, interactRange, interactLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out IInteractable interactableCounter))
+            {
+                //  ÕœÌÀ «·‹ Selected Counter
+                if (SelectedCounter != interactableCounter)
+                {
+                    SetSelectedCounter(interactableCounter);
+                }
+            }
+            else
+            {
+                SetSelectedCounter(null);
+            }
+        }
+        else
+        {
+            SetSelectedCounter(null);
+        }
+
+        //  ‰›Ì– «· ›«⁄· „⁄ «·‹ Selected Counter
+        if (SelectedCounter != null)
+        {
+            SelectedCounter.Interact(this);
+        }
+    }
+    private void HandelInteractAlternate()
+    {
+        if (moveInput != Vector2.zero)
+        {
+            lastIntaractinDir = transform.forward;
+        }
+        
+
+        if (Physics.Raycast(transform.position, lastIntaractinDir, out RaycastHit raycastHit, interactRange, interactLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out IInteractable interactableCounter))
+            {
+                interactableCounter.InteractAlterante(this);
+                Debug.Log($"Interact Alternate with: {raycastHit.transform.name}");
+            }
+        }
     }
 
     private void HandleMovement()
@@ -62,6 +136,10 @@ public class PlayerMovement : MonoBehaviour
 
         float targetSpeed = isCrouching ? crouchSpeed : (isSprinting ? sprintSpeed : walkSpeed);
         Vector3 targetVelH = planarMoveDir * targetSpeed;
+
+        float RotationSpeed = 10f;
+        Vector3 moveDir = new Vector3(targetVelH.x, 0f, targetVelH.z);
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * RotationSpeed);
 
         Vector3 v = rb.linearVelocity;
         Vector3 vH = Vector3.Lerp(new Vector3(v.x, 0f, v.z), targetVelH, acceleration * Time.fixedDeltaTime);
@@ -83,11 +161,13 @@ public class PlayerMovement : MonoBehaviour
         Gizmos.DrawLine(transform.position, transform.position + Vector3.down * groundDistanceCheck);
     }
 
+   
     #region Input System Callbacks
 
     public void OnMove(InputAction.CallbackContext ctx)
     {
         moveInput = ctx.ReadValue<Vector2>();
+
     }
 
     public void OnJump(InputAction.CallbackContext ctx)
@@ -134,5 +214,69 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+
+    [SerializeField] private float interactCooldown = 0.15f;
+    private float nextInteractTime = 0f;
+    // ...
+    public void OnInteract(InputAction.CallbackContext ctx)
+    {
+        if (!ctx.performed) return;
+        if (Time.time < nextInteractTime) return;   //  »—Ìœ »”Ìÿ
+        nextInteractTime = Time.time + interactCooldown;
+
+        HandelInteraction(); // «” œ⁄«¡ Ê«Õœ ‰ŸÌ›
+    }
+
+
+    public void OnInteractAlternate(InputAction.CallbackContext ctx)
+    {
+        Debug.Log($"Interact Alternate pressed: {ctx.phase}");
+
+        if (!ctx.performed) return;
+        if (Time.time < nextInteractTime) return;
+        nextInteractTime = Time.time + interactCooldown;
+
+        Debug.Log("Handling Interact Alternate");
+        HandelInteractAlternate();
+    }
+
+
+
+    private void SetSelectedCounter(IInteractable selectedCounter)
+    {
+        this.SelectedCounter = selectedCounter;
+        OnSelectedCounterChanged?.Invoke(this, new SelectedCounterChangedEventArgs
+        {
+            selectedCounter = SelectedCounter
+        });
+    }
+    private void OnDrawGizmosSelected()
+    {
+        if (cameraTransform == null) return;
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(cameraTransform.position, cameraTransform.position + cameraTransform.forward * interactRange);
+    }
+
+    public Transform GetKitchenObjectFollowTransform() => holdPoint;
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
+    }
     #endregion
 }
