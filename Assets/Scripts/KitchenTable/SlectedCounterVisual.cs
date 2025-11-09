3c83b52703c1b528c96a0ff4a9c57676c77598ff
Alreem Alothman <alreemothman66@gmail.com>
2025-11-09 16:26:47 +0300

    some conflict

--- Assets/Scripts/KitchenTable/SlectedCounterVisual.cs
+++ Assets/Scripts/KitchenTable/SlectedCounterVisual.cs
@@ -7,24 +7,24 @@ public class SlectedCounterVisual : MonoBehaviour
    
     private void Start()
     {
-        //PlayerMovement playerMovement = FindFirstObjectByType<PlayerMovement>();
-        //if (playerMovement != null)
-        //{
-        //    playerMovement.OnSelectedCounterChanged += Player_OnSelectedCounterChanged;
-        //}
+        PlayerMovement playerMovement = FindFirstObjectByType<PlayerMovement>();
+        if (playerMovement != null)
+        {
+            playerMovement.OnSelectedCounterChanged += Player_OnSelectedCounterChanged;
+        }
     }
 
-    //private void Player_OnSelectedCounterChanged(object sender, PlayerMovement.SelectedCounterChangedEventArgs e)
-    //{
-    //    if (e.selectedCounter == baseCounter)
-    //    {
-    //        Show();
-    //    }
-    //    else
-    //    {
-    //        Hide();
-    //    }
-    //}
+    private void Player_OnSelectedCounterChanged(object sender, PlayerMovement.SelectedCounterChangedEventArgs e)
+    {
+        if (e.selectedCounter == baseCounter)
+        {
+            Show();
+        }
+        else
+        {
+            Hide();
+        }
+    }
 
     private void Show()
     {
