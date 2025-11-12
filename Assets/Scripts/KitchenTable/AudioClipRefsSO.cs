24c2fec8de7c0065259b9593fed917e8c8e7c5c9
Alreem Alothman <alreemothman66@gmail.com>
2025-11-13 01:47:21 +0300

    ok

---
+++ Assets/Scripts/KitchenTable/AudioClipRefsSO.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class AudioClipRefsSO : ScriptableObject
+{
+    public AudioClip[] chope;
+    public AudioClip[] delivartfail;
+    public AudioClip[] delivartsuccess;
+    public AudioClip[] footstap;
+    public AudioClip[] ObjectDrop;
+    public AudioClip[] ObjectPickup;
+    public AudioClip StoveSizzle;
+    public AudioClip[] trash;
+    public AudioClip[] warning;
+
+}
