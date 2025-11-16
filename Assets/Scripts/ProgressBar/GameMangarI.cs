a33e6b063161b8c77c0265e96a7e4388aa3f2262
Alreem Alothman <alreemothman66@gmail.com>
2025-11-16 17:45:15 +0300

    new5.2

---
+++ Assets/Scripts/ProgressBar/GameMangarI.cs
@@ -0,0 +1,70 @@
+using System;
+using UnityEngine;
+
+public class GameMangarI : MonoBehaviour
+{
+    public static GameMangarI Instance { get; private set; }
+
+    public event EventHandler OnStateChanged;
+    private enum State
+    {
+        WaitingToStsrt,
+        CounrdownToStart,
+        GamePlaying,
+        GameOver,
+    }
+
+    private State state;
+    private float waitingToStartTimer = 1f;
+    private float countDownToStartTimer = 3f;
+    private float gamePlayingTimer = 20f;
+
+    private void Awake()
+    {
+        Instance = this;
+        state = State.WaitingToStsrt;
+    }
+
+    private void Update()
+    {
+        switch (state)
+        {
+            case State.WaitingToStsrt:
+                waitingToStartTimer -= Time.deltaTime;
+                if (waitingToStartTimer <= 0f)
+                {
+                    state = State.CounrdownToStart;
+                    OnStateChanged?.Invoke(this, EventArgs.Empty);
+                }
+                break;
+            case State.CounrdownToStart:
+                countDownToStartTimer -= Time.deltaTime;
+                if (countDownToStartTimer <= 0f)
+                {
+                    state = State.GamePlaying; // Transition to GamePlaying for demonstration
+                    OnStateChanged?.Invoke(this, EventArgs.Empty);
+                }
+                break;
+            case State.GamePlaying:
+                gamePlayingTimer -= Time.deltaTime;
+                if (gamePlayingTimer <= 0f)
+                {
+                    state = State.GameOver;
+                    OnStateChanged?.Invoke(this, EventArgs.Empty);
+                }
+                break;
+            case State.GameOver:
+                // Implement game over logic here
+                break;
+        }
+    }
+
+
+
+    public bool IsGamePlaying()
+    {
+        return state == State.GamePlaying;
+    }
+}
+
+
---
+++ Assets/Scripts/_UI/GameStartCountDown.cs
@@ -0,0 +1,8 @@
+using TMPro;
+using UnityEngine;
+
+public class GameStartCountDown : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI countdownText;
+    [SerializeField] private float countdownTime = 3f;
+}
